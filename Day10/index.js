75b1f9e4d7d06b04aa1752c0971afb40e7ca0204
eeswari-s <eeswarivaithee@gmail.com>
2025-11-27 15:25:45 +0530

    c

---
+++ Day10/index.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+//  let login = "user";
+
+// if(login === "user")
+// {
+//     console.log("redirect to dashboard");
+// }
+// else{
+//     console.log("user not found");
+// }
+
+let login = "harish"; 
+
+ let employee = login === "user" ? "red" : "not found";
+ console.log(employee); 
+
---
+++ Day9/index.js
@@ -0,0 +1,49 @@
+console.log("welcome");
+
+console.table(["welcome","hello","javascript"]);
+
+
+console.group("message");
+console.log("recieved");
+console.groupEnd();
+
+// alert("sorry not found");
+
+// prompt("what is your name: ");
+
+// console.info("welcome to our page");
+
+var sugar =500;
+console.log("sugar: "+ sugar);
+
+
+
+let salt="1kg";
+console.log("salt: "+ salt);
+
+const phone=8765;
+console.log("phone : "+ phone);
+
+
+var person1="sugar5";
+
+
+var person1="sugar5";
+
+ person1="salt"
+
+console.log(person1);
+console.log(person1);
+
+
+let a=10;
+console.log(a);
+ a=15;
+console.log(a);
+
+
+const b=5;
+console.log(b);
+
+const  c=8;
+console.log(c);
